416aeffb231b2637a94314d01bf426cfe1629653
RomanV <RV@gmail.com>
2019-12-19 18:34:37 +0300

    обновление

--- RestWebApp/src/org/rest/util/XMLCurrencyParser.java
+++ RestWebApp/src/org/rest/util/XMLCurrencyParser.java
@@ -1,8 +1,13 @@
 package org.rest.util;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+
+import org.rest.ws.model.Currency;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -14,7 +19,29 @@ public class XMLCurrencyParser
 		  public XMLCurrencyParser() {}
 		  
 		  private static String CURRENCY_URL = "http://www.nbrb.by/Services/XmlExRates.aspx";
-		  
+		  private static List<Currency> currencies = new ArrayList<Currency>();
+		  private static Document doc=null;
+		  static {
+			  while (doc==null) {
+				  try {
+				  Thread.sleep(1000);
+				  }catch(InterruptedException e){
+					  e.printStackTrace();
+				  }
+				  refresh(); //получение документа
+			  }
+			   
+		  }
+		  /*
+		   * Load по требованию
+		   */
+		  public static void refresh() {
+			  doc = loadDocument(CURRENCY_URL);
+			  if(doc != null) {
+		          System.out.println("The Document is loaded!");
+		      }
+			  
+		  }
 		  private static Document loadDocument(String url) {
 		    Document doc = null;
 		    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -35,7 +62,7 @@ public class XMLCurrencyParser
 		    }
 		    return doc;
 		  }
-		  public static String getCurrency(String currencyCode)
+		 public static String getCurrency(String currencyCode)
 		  {
 		    boolean isCurrencyCodeNext = false;
 		    Document doc = loadDocument(CURRENCY_URL);
@@ -68,8 +95,79 @@ public class XMLCurrencyParser
 		    }
 		    return "0.0";
 		  }
+		 public static void getCurrencies() {
+			 NodeList nodes = doc.getFirstChild().getChildNodes();//загрузка п структуре документа XML 1Chaild-listCurrecn
+			 Currency cur = new Currency();
+			 for (int i = 0; i < nodes.getLength(); i++) {
+			        Node parent = nodes.item(i);
+			        if (parent.getNodeType() == Node.ELEMENT_NODE) {
+			          NodeList childs = parent.getChildNodes();
+			          
+			          for (int j = 0; j < childs.getLength(); j++) {
+			        	  Node child = childs.item(j);
+			        	  if(child.hasChildNodes()) {
+			        		  if (child.getNodeName().trim().equalsIgnoreCase("NumCode")){ 
+			        		  cur.setNumCode(child.getNodeName());
+			        		  break;}
+			        		  if (child.getNodeName().trim().equalsIgnoreCase("CharCode")){ 
+			        		  cur.setCharCode(child.getNodeName());
+			        		  break;}
+			        		  if (child.getNodeName().trim().equalsIgnoreCase("Scale")){ 
+			        		  cur.setScale(Integer.parseInt(child.getNodeName()));
+			        		  break;}
+			        		  if (child.getNodeName().trim().equalsIgnoreCase("Name")){ 
+			        		  cur.setName(child.getNodeName());
+			        		  break;}
+			        		  if (child.getNodeName().trim().equalsIgnoreCase("Rate")){ 
+			        			  cur.setRate(Double.parseDouble(child.getNodeName()));
+			        			  break;
+			        		  }
+
+			        		  }
+			        	  }
+			          System.out.println(cur);
+			        	  
+			        	  
+			          }
+			        }
+			 }
+			 
+		 
+		 public static Currency getCurrencyObject(String currencyCode)
+		  {
+			 Currency cur = new Currency();
+			 boolean isCurrencyCodeNext = false;
+		    
+		    if (doc != null) {
+		      NodeList nodes = doc.getFirstChild().getChildNodes();//загрузка п структуре документа XML 1Chaild-listCurrecn
+
+		      for (int i = 0; i < nodes.getLength(); i++) {
+		        Node parent = nodes.item(i);
+		        
+		        if (parent.getNodeType() == 1) {
+		          NodeList childs = parent.getChildNodes();
+		          
+		          for (int ii = 0; ii < childs.getLength(); ii++) {
+		            Node child = childs.item(ii);
+		            if (child.hasChildNodes()) {
+		              if ((child.getNodeName().trim().equalsIgnoreCase("Rate")) && (isCurrencyCodeNext)) {
+		                isCurrencyCodeNext = false;
+		                return cur;
+		              }
+		              
+		              if (child.getFirstChild().getTextContent().trim().equalsIgnoreCase(currencyCode)) {
+		                isCurrencyCodeNext = true;
+		              }
+		            }
+		          }
+		        }
+		      }
+		    }
+		    return cur;
+		  }
 public static void main(String[] args) {
 System.out.println(getCurrency("840"));
+getCurrencies();
 }
 	}
 
--- RestWebApp/src/org/rest/ws/core/TestService.java
+++ RestWebApp/src/org/rest/ws/core/TestService.java
@@ -1,35 +1,34 @@
 package org.rest.ws.core;
 
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
-import org.rest.ws.model.Book;
-import org.rest.ws.model.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-
-
-
-
-@Path("/book")
-@RestController
+import org.rest.ws.model.Person;
+@Path("person")
 public class TestService {
-
-	
-	@Autowired
-	   private BookService bookService;
 	
 	@GET
-	@GetMapping("/{id}")
-	public ResponseEntity<Book> getMsg(@PathVariable("id") int id) {
-
-		Book book = bookService.get(id);
-
-		 return ResponseEntity.ok().body(book);
-
+	@Path("/{personId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Person getPerson(@PathParam("personId") int id) {
+		Person p = new Person();
+		
+		p.setName("Roman");
+		p.setId(id);
+		return p;
+	}
+	@POST
+	@Path("/update")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Person updatePerson(Person personToUpdate) {
+		personToUpdate.setName(personToUpdate.getName().toUpperCase());
+		return personToUpdate;
 	}
 }
